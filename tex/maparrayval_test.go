e124e8572df0aea158d05093eb608373c4d8e96d
zhangkun <>
2021-10-26 16:18:48 +0800

    add map val convert to basic list

---
+++ tex/maparrayval_test.go
@@ -0,0 +1,108 @@
+package tex
+
+import (
+	"github.com/pinealctx/neptune/jsonx"
+	"github.com/vmihailenco/msgpack/v5"
+	"reflect"
+	"testing"
+)
+
+type X struct {
+	A int `msgpack:"a"`
+}
+
+func TestMapMarshal(t *testing.T) {
+	var m = make(map[string]interface{})
+	m["a"] = []string{"1", "2", "3"}
+	m["b"] = []int64{1, 2, 3}
+	m["c"] = []X{{A: 1}, {A: 2}}
+	var buf, err = msgpack.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+	var x map[string]interface{}
+	err = msgpack.Unmarshal(buf, &x)
+	if err != nil {
+		panic(err)
+	}
+	for k, v := range x {
+		t.Log("k", k)
+		t.Log("v", v)
+		t.Log("v type:", reflect.TypeOf(v))
+	}
+
+	buf, err = jsonx.JSONFastMarshal(m)
+	if err != nil {
+		panic(err)
+	}
+	var y map[string]interface{}
+	err = jsonx.JSONFastUnmarshal(buf, &y)
+	if err != nil {
+		panic(err)
+	}
+	for k, v := range y {
+		t.Log("k", k)
+		t.Log("v", v)
+		t.Log("v type:", reflect.TypeOf(v))
+	}
+}
+
+func TestMapArray(t *testing.T) {
+	var m = make(map[string]interface{})
+	m["a"] = []string{"1", "2", "3"}
+	m["b"] = []int64{1, 2, 3}
+	m["c"] = []X{{A: 1}, {A: 2}}
+	var buf, err = msgpack.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+	var x map[string]interface{}
+	err = msgpack.Unmarshal(buf, &x)
+	if err != nil {
+		panic(err)
+	}
+
+	var (
+		i64s []int64
+		ss   []string
+		ok   bool
+	)
+
+	ss, ok = MapVal2StringList(x, "a")
+	if !ok {
+		panic(ss)
+	}
+	t.Log(ss)
+	t.Log(reflect.TypeOf(ss))
+
+	i64s, ok = MapVal2Int64List(x, "b")
+	if !ok {
+		panic(i64s)
+	}
+	t.Log(i64s)
+	t.Log(reflect.TypeOf(i64s))
+
+	buf, err = jsonx.JSONFastMarshal(m)
+	if err != nil {
+		panic(err)
+	}
+	var y map[string]interface{}
+	err = jsonx.JSONFastUnmarshal(buf, &y)
+	if err != nil {
+		panic(err)
+	}
+
+	ss, ok = MapVal2StringList(y, "a")
+	if !ok {
+		panic(ss)
+	}
+	t.Log(ss)
+	t.Log(reflect.TypeOf(ss))
+
+	i64s, ok = MapVal2Int64List(y, "b")
+	if !ok {
+		panic(i64s)
+	}
+	t.Log(i64s)
+	t.Log(reflect.TypeOf(i64s))
+}
