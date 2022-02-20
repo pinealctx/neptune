e82cf8fce929930692b1ad50a917d572d472670f
zhangkun <>
2022-02-20 17:11:43 +0800

    add more async package
    more easily use async call/proc/delegate package

--- syncx/pipe/async/procchan_test.go
+++ syncx/pipe/async/procchan_test.go
@@ -42,18 +42,37 @@ func TestProcChan_Call(t *testing.T) {
 }
 
 type _incChan struct {
-	x int
-	r chan int
+	x  int
+	rc chan int
 }
 
-func (i *_incChan) Do() {
+func (i *_incChan) do() {
 	i.x++
-	i.r <- i.x
+	i.rc <- i.x
 }
 
-func (i *_incChan) R() int {
-	var x = <-i.r
+func (i *_incChan) r() int {
+	var x = <-i.rc
 	return x
+}
+
+func (i *_incChan) doCtx(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		i.x++
+		close(i.rc)
+	}
+}
+
+func (i *_incChan) rCtx(ctx context.Context) int {
+	select {
+	case <-ctx.Done():
+		return 0
+	case <-i.rc:
+		return i.x
+	}
 }
 
 func TestChan_Call(t *testing.T) {
@@ -63,18 +82,46 @@ func TestChan_Call(t *testing.T) {
 		var i *_incChan
 		for {
 			i = <-cc
-			i.Do()
+			i.do()
 		}
 	}()
 	var xs = make([]*_incChan, size)
 	for i := 0; i < size; i++ {
-		xs[i] = &_incChan{x: i, r: make(chan int)}
+		xs[i] = &_incChan{x: i, rc: make(chan int)}
 	}
 
 	var t1 = time.Now()
 	for i := 0; i < size; i++ {
 		cc <- xs[i]
-		xs[i].R()
+		xs[i].r()
+	}
+	var t2 = time.Now()
+	var dur = t2.Sub(t1)
+	t.Log("use time:", dur, "average:", dur/time.Duration(size))
+}
+
+func TestChan_CallCtx(t *testing.T) {
+	var size = DefaultQSize * 100
+	var cc = make(chan *_incChan, DefaultQSize)
+	var ctx, cancel = context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	go func() {
+		var i *_incChan
+		for {
+			i = <-cc
+			i.doCtx(ctx)
+		}
+	}()
+	var xs = make([]*_incChan, size)
+	for i := 0; i < size; i++ {
+		xs[i] = &_incChan{x: i, rc: make(chan int)}
+	}
+
+	var t1 = time.Now()
+	for i := 0; i < size; i++ {
+		cc <- xs[i]
+		xs[i].rCtx(ctx)
 	}
 	var t2 = time.Now()
 	var dur = t2.Sub(t1)
