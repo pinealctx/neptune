ba1538f143d6324980d470bb749de4412f5dfd2b
zhangkun <zhangkun907@outlook.com>
2022-11-18 14:49:09 +0800

    Wrap time.Timer with safety Stop/Reset.

---
+++ timex/timer.go
@@ -0,0 +1,46 @@
+package timex
+
+import "time"
+
+type Timer struct {
+	*time.Timer
+}
+
+// NewTimer creates a new Timer that will send
+// the current time on its channel after at least duration d.
+func NewTimer(d time.Duration) Timer {
+	return Timer{time.NewTimer(d)}
+}
+
+// Stop : stop timer
+// In time.Timer, Stop can not ensure drain the timer channel.
+// Caller should use external code.
+//
+// 	if !t.Stop() {
+// 		<-t.C
+// 	}
+//
+// Actually, it the t.C be read from others, the outside caller should avoid drain the timer channel again.
+// For instance, time handler be called so that t.C is read then it's empty.
+//
+// This code will be blocked here forever because t.C is empty now.
+// 	if !t.Stop() {
+// 		<-t.C
+// 	}
+//
+//  The function use "select with default" to avoid this case.
+func (x Timer) Stop() {
+	if !x.Timer.Stop() {
+		select {
+		case <-x.C:
+		default:
+		}
+	}
+}
+
+// Reset changes the timer to expire after duration d.
+// This function first call "Stop" to make sure t.C drained
+func (x Timer) Reset(d time.Duration) {
+	x.Stop()
+	x.Timer.Reset(d)
+}
---
+++ timex/timer_test.go
@@ -0,0 +1,79 @@
+package timex
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTimerStop1(t *testing.T) {
+	procTest(t, testTimerStop1)
+}
+
+func testTimerStop1(t *testing.T) {
+	var x = NewTimer(time.Second * 2)
+	var tt, ok = <-x.C
+	t.Log(tt, ok)
+	x.Stop()
+	x.Stop()
+}
+
+func TestTimerStop2(t *testing.T) {
+	procTest(t, testTimerStop2)
+}
+
+func testTimerStop2(t *testing.T) {
+	var x = NewTimer(time.Second * 2)
+	x.Stop()
+	x.Stop()
+}
+
+func TestTimerReset1(t *testing.T) {
+	procTest(t, testTimerReset1)
+}
+
+func testTimerReset1(t *testing.T) {
+	var x = NewTimer(time.Second * 2)
+	var tt, ok = <-x.C
+	t.Log(tt, ok)
+	x.Reset(time.Second * 3)
+	tt, ok = <-x.C
+	t.Log(tt, ok)
+	x.Stop()
+}
+
+func TestTimerReset2(t *testing.T) {
+	procTest(t, testTimerReset2)
+}
+
+func testTimerReset2(t *testing.T) {
+	var x = NewTimer(time.Second * 2)
+	var tt, ok = <-x.C
+	t.Log(tt, ok)
+	x.Reset(time.Second * 3)
+	x.Reset(time.Second * 2)
+	x.Reset(time.Second * 1)
+	tt, ok = <-x.C
+	t.Log(tt, ok)
+	x.Stop()
+}
+
+func TestTimerReset3(t *testing.T) {
+	procTest(t, testTimerReset3)
+}
+
+func testTimerReset3(t *testing.T) {
+	var x = NewTimer(time.Second * 2)
+	x.Stop()
+	x.Reset(time.Second * 3)
+	x.Reset(time.Second * 2)
+	x.Reset(time.Second * 1)
+	var tt, ok = <-x.C
+	t.Log(tt, ok)
+	x.Stop()
+}
+
+func procTest(t *testing.T, f func(*testing.T)) {
+	t.Log(time.Now(), "begin")
+	f(t)
+	t.Log(time.Now(), "end")
+}
