20c05d4761e77af70e95fcf069cab4f7de0b6a6a
Silkage <silkage@silkage.net>
2024-04-16 10:30:08 +0800

    fix: add slice funcs

--- genericx/slicex/slice.go
+++ genericx/slicex/slice.go
@@ -1,5 +1,11 @@
 package slicex
 
+import (
+	"cmp"
+	"math/rand"
+	"time"
+)
+
 // Insert 往slice里面插入元素，如果index大于slice的长度，则追加在最后面，如果index小于0，则panic
 func Insert[T any](s *[]T, index int, v T) {
 	if index >= len(*s) {
@@ -53,4 +59,80 @@ func FindIndex[T comparable](s []T, v T) int {
 // Contain 是否包含指定元素
 func Contain[T comparable](s []T, v T) bool {
 	return FindIndex[T](s, v) != -1
+}
+
+// ContainFunc 是否包含指定元素
+func ContainFunc[T any](s []T, f func(T) bool) bool {
+	for _, v := range s {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// Clone 克隆一个slice
+func Clone[T any](list []T) []T {
+	return append([]T(nil), list...)
+}
+
+type Number interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
+}
+
+// Sum 求和
+func Sum[T Number](list []T) T {
+	var sum T
+	for _, v := range list {
+		sum += v
+	}
+	return sum
+}
+
+// Max 求最大值
+func Max[T cmp.Ordered](list []T) T {
+	l := len(list)
+	if l == 0 {
+		panic("empty list")
+	}
+	var maxV = list[0]
+	for i := 1; i < l; i++ {
+		if list[i] > maxV {
+			maxV = list[i]
+		}
+	}
+	return maxV
+}
+
+// Min 求最小值
+func Min[T cmp.Ordered](list []T) T {
+	l := len(list)
+	if l == 0 {
+		panic("empty list")
+	}
+	var minV = list[0]
+	for i := 1; i < l; i++ {
+		if list[i] < minV {
+			minV = list[i]
+		}
+	}
+	return minV
+}
+
+var (
+	shuffleRand *rand.Rand
+)
+
+// Shuffle shuffles a list
+func Shuffle[T any](list []T) []T {
+	if shuffleRand == nil {
+		shuffleRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	}
+	for i := len(list) - 1; i > 0; i-- {
+		j := shuffleRand.Intn(i + 1)
+		list[i], list[j] = list[j], list[i]
+	}
+	return list
 }
