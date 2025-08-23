994d6f7a1983f268064d50423ca3614680fc628e
zhangkun <zhangkun907@outlook.com>
2025-08-23 23:41:42 +0800

    golangci-lint log.

--- ulog/log_test.go
+++ ulog/log_test.go
@@ -13,36 +13,43 @@ func TestNewZapLoggerX1(_ *testing.T) {
 }
 
 func test1() {
+	fs1 := []zap.Field{
+		zap.Duration("d", time.Second),
+		zap.Int("i", 1),
+		zap.Time("ti", time.Now()),
+		zap.ByteString("bs", []byte("abc我")),
+	}
+	fs2 := []zap.Field{
+		zap.Duration("d", time.Millisecond),
+		zap.Int("i", 1),
+		zap.Time("ti", time.Now()),
+		zap.ByteString("bs", []byte("abc我")),
+		zap.Stack("stack"),
+	}
+	fs3 := []zap.Field{
+		zap.Duration("d", time.Microsecond),
+		zap.Int("i", 1),
+		zap.Time("ti", time.Now()),
+		zap.ByteString("bs", []byte("abc我")),
+	}
+	fs4 := []zap.Field{
+		zap.Duration("d", time.Nanosecond),
+		zap.Int("i", 1),
+		zap.Time("ti", time.Now()),
+		zap.ByteString("bs", []byte("abc我")),
+	}
 	defer func() {
 		var logger = NewSimpleLogger("info", zap.AddCaller(), zap.AddCallerSkip(1))
-		logger.Debug("debug",
-			zap.Duration("d", time.Second),
-			zap.Int("i", 1),
-			zap.Time("ti", time.Now()),
-			zap.ByteString("bs", []byte("abc我")),
-		)
-		logger.Info("info",
-			zap.Duration("d", time.Millisecond),
-			zap.Int("i", 1),
-			zap.Time("ti", time.Now()),
-			zap.ByteString("bs", []byte("abc我")),
-			zap.Stack("stack"),
-		)
-		logger.Warn("warn",
-			zap.Duration("d", time.Second),
-			zap.Int("i", 1),
-			zap.Time("ti", time.Now()),
-			zap.ByteString("bs", []byte("abc我")),
-			zap.Error(nil),
-		)
-		logger.Error("err",
-			zap.Duration("d", time.Second),
-			zap.Int("i", 1),
-			zap.Time("ti", time.Now()),
-			zap.ByteString("bs", []byte("abc我")),
-			zap.Error(errors.New("error1")),
-		)
+		logger.Debug("debug.defer", fs1...)
+		logger.Info("info.defer", fs2...)
+		logger.Warn("warn.defer", fs3...)
+		logger.Error("err.defer", fs4...)
 	}()
+	var logger = NewSimpleLogger("info")
+	logger.Debug("debug", fs1...)
+	logger.Info("info", fs2...)
+	logger.Warn("warn", fs3...)
+	logger.Error("err", fs4...)
 }
 
 func TestNewZapLoggerX2(_ *testing.T) {
