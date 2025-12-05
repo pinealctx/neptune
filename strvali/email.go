1043a7a957bbab046758b364127769970f30b81b
kun <kun@xsyphon.com>
2025-12-05 11:16:10 +0800

    Add email "NormalizeEmail", strings.ToLower/strings.TrimSpace.
    Update go.mod.

--- strvali/email.go
+++ strvali/email.go
@@ -2,13 +2,20 @@ package strvali
 
 import (
 	"regexp"
+	"strings"
 )
 
 var (
 	emailReg = regexp.MustCompile(`^\w[-\w.+]*@([A-Za-z0-9][-_A-Za-z0-9]*\.)+[A-Za-z]{2,14}$`)
 )
 
-// IsValidEmail 验证是否为合法的邮箱
+// IsValidEmail checks if the provided email string is a valid email format
 func IsValidEmail(email string) bool {
 	return emailReg.MatchString(email)
+}
+
+// NormalizeEmail converts email to lowercase
+// Email addresses are case-insensitive according to RFC 5321
+func NormalizeEmail(email string) string {
+	return strings.ToLower(strings.TrimSpace(email))
 }
